0c3d1e63594cecd62159715acdf72d2c23d24e43
crocodileDaniil <karasev_dan_an@mail.ru>
2023-10-08 12:36:14 +0300

    module_2-all

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let summ = 0;
+  for( key in salaries) {
+    if(isFinite(salaries[key])) {
+      summ += salaries[key]
+    }
+  }
+  return summ
 }
