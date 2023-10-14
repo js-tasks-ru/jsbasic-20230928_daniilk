5234027f5cf9b260b4d8d6b93d962374cee69b6b
crocodileDaniil <karasev_dan_an@mail.ru>
2023-10-14 15:52:53 +0300

    module-3_task-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,33 @@
 function getMinMax(str) {
-  // ваш код...
+  // что из этого лучше?
+  // let arr = str.split(' ').filter(elem => isFinite(elem)).map(elem => +elem)
+  // return {
+  //   min: Math.min(...arr),
+  //   max: Math.max(...arr)
+  // }
+
+  return maxMin(
+    str
+      .split(" ")
+      .filter((elem) => isFinite(elem))
+      .map((elem) => +elem)
+  );
+}
+
+function maxMin(arr) {
+  let max = arr[0];
+  let min = arr[0];
+
+  for (elem of arr) {
+    if (elem > max) {
+      max = elem;
+    }
+    if (elem < min) {
+      min = elem;
+    }
+  }
+  return {
+    min,
+    max,
+  };
 }
