5974d2206687b04a42678b3934ed2fad63eaef48
crocodileDaniil <karasev_dan_an@mail.ru>
2023-10-18 00:03:13 +0300

    module-4_task-3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,34 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.tBodies[0].rows;
+  console.log(rows);
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
+    let atrrDataAvail = row.cells[3].getAttribute("data-available");
+    let gender = row.cells[2].innerHTML;
+    let age = Number(row.cells[1].innerHTML)
+
+// Почему не работает проверка этим способом и даже если убирать пробелы? для male и female подходит любой 
+      // atrrDataAvail === "true"
+      // ? (row.className = "available ")
+      // : atrrDataAvail === "false"
+      // ? (row.className = "unavailable ")
+      // : (row.hidden = true );
+
+      atrrDataAvail === "true"
+      ? (row.classList.add("available"))
+      : atrrDataAvail === "false"
+      ? (row.classList.add("unavailable"))
+      : (row.hidden = true );
+
+    gender === "m"
+      ? (row.className += " male")
+      : (row.className += " female");
+
+      // gender === "m"
+      // ? row.classList.add("male")
+      // : row.classList.add("female");
+
+      if (age < 18) row.style.textDecoration = 'line-through'
+
+  }
 }
