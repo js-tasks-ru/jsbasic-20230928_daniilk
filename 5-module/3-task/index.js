bb47ece7cdb5d4dabdd2a25fc808b8a22063d06b
crocodileDaniil <karasev_dan_an@mail.ru>
2023-10-23 00:16:26 +0300

    Module5_task-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let containerClick = document.querySelector(".carousel");
+  let i = 0;
+  document.querySelector(".carousel__arrow_left").style.display = "none";
+  containerClick.addEventListener("click", (event) => {
+    let container = document.querySelector(".carousel__inner");
+    let buttonSwipe = event.target.closest("div");
+    let swipe = document.querySelector(".carousel__slide");
+
+    switch (buttonSwipe.className) {
+      case "carousel__arrow carousel__arrow_right":
+        document.querySelector(".carousel__arrow_left").style.display = "";
+        i += 1;
+        container.style.transform = `translateX(-${swipe.offsetWidth * i}px)`;
+        if (i == 3) {
+          buttonSwipe.style.display = "none";
+        }
+        break;
+
+      case "carousel__arrow carousel__arrow_left":
+        document.querySelector(".carousel__arrow_right").style.display = "";
+        i -= 1;
+        container.style.transform = `translateX(-${swipe.offsetWidth * i}px)`;
+        if (i == 0) {
+          buttonSwipe.style.display = "none";
+        }
+        break;
+    }
+  });
 }
